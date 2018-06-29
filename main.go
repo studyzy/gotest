83a2a6990383c972733b92e9f6931a35a74602cc
Devin <studyzy@gmail.com>
2018-06-29 18:50:34 +0800

    add CI build.

--- main.go
+++ main.go
@@ -5,9 +5,13 @@ import (
 
 	"github.com/facebookgo/inject"
 	"github.com/studyzy/gotest/consensus"
+	_ "github.com/studyzy/gotest/core"
 	"github.com/studyzy/gotest/dag"
 )
 
+type Address [32]byte
+
+func (a Address) Hex() string { return fmt.Sprintf("0x%x", a) }
 func main() {
 	fmt.Println("Hello World!")
 
@@ -20,4 +24,7 @@ func main() {
 	g.Populate()
 
 	fmt.Println(m.ReadSystemConfig("Count"))
+	var a = new(Address)
+	a[0] = byte('1')
+	fmt.Println(a.Hex())
 }
